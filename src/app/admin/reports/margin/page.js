b0bd1b22c32d68552f4311363c31c142d2e519fb
vignesh-trivecta <129928465+vignesh-trivecta@users.noreply.github.com>
2023-09-22 18:54:41 +0530

    Update page.js

--- src/app/admin/reports/margin/page.js
+++ src/app/admin/reports/margin/page.js
@@ -1,64 +1,106 @@
-'use client';
+"use client";
 
-import { useState, useEffect } from 'react';
-import ExportRow from '@/components/page/exportRow';
-import FilterComponent from '@/components/page/filterComp';
-import ReportsTable from '@/components/admin/reportsTable';
-import { useSelector } from 'react-redux';
-import { handleFetchReports } from '@/app/api/reports/route';
-import print from 'print-js';
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { handleDbReportsFetch } from "@/app/api/reports/route";
+import ExportRow from "@/components/page/exportRow";
+import FilterComponent from "@/components/page/filterComp";
+import ReportsTable from "@/components/admin/reportsTable";
+import print from "print-js";
+import FilteredData from "@/components/admin/filteredData";
+import Breadcrumbs from "@/components/page/breadcrumb";
 
 const Margin = () => {
 
   // local state
-  const [data, setData] = useState([]);
+  const [datas, setDatas] = useState([]);
 
+  // redux
+  const customerId = useSelector((state) => state.report.customerId);
+  const broker = useSelector((state) => state.report.broker);
+  const reportType = useSelector((state) => state.report.reportType);
+
+  const startDate = useSelector((state) => state.report.startDate);
+  const endDate = useSelector((state) => state.report.endDate);
+  const toggle = useSelector((state) => state.report.toggle);
+
+  // table to PDF
   const printTableToPDF = () => {
-    const tableId = 'table-to-print';
-    printJS({ printable: tableId, type: 'html',style: 'Td { border: 1px solid #D1D5DB !important;} Th { border: 1px solid #D1D5DB !important;}' });
+    const tableId = "table-to-print";
+    printJS({
+      printable: tableId,
+      type: "html",
+      style:
+        "Td { border: 1px solid #D1D5DB !important;} Th { border: 1px solid #D1D5DB !important;}",
+    });
   };
 
-  const datas = [
-    {
-      "ALB": 317.22, 
-      "AvailableMargin": 317.22, 
-      "GHV": 0, 
-      "GrossMargin": 317.22, 
-    }
-  ]
+  const ids = [{ Reports: "/admin/reports" }, { Margin: "" }];
 
-  const customerId = useSelector((state) => state.report.customerId);
-  const columns = Object.keys(datas[0]);
+  const columns = ["Adjusted Ledger Balance ₹", "Gross Holding Value ₹", "Gross Margin ₹", "Available Margin ₹"]
 
+  // useEffect to fetch table data from backend
   useEffect(() => {
     const fetchMargin = async () => {
-      const response = await handleFetchReports("margin", customerId);
-      console.log(response);
-      // setData(response);
-    }
+      if (reportType === "Market") { // Live market data endpoint
+        console.log('market')
+        const response = await handleLiveReportsFetch(
+          "margin",
+          customerId,
+          startDate,
+          endDate
+        );
+        console.log(response);
+        // setDatas(response);
+      }
+      else if (reportType === "Post") { // DB data endpoint
+        console.log('post')
+        const response = await handleDbReportsFetch(
+          "margin",
+          customerId,
+          startDate,
+          endDate
+        );
+        console.log(response);
+        setDatas(response);
+      }
+      else {
+        setDatas([])
+      }
+    };
     fetchMargin();
-  }, []);
+  }, [toggle]);
 
   return (
-    <div className='container mx-auto mt-4 h-full' style={{width: '95%'}}>
-      <div className=' flex justify-between'>
-        <h1 className="font-bold">Margin</h1>
-        <div className='flex justify-end space-x-2'>
-            <div className='relative'>
-              <FilterComponent />
-            </div>
-            <div>
-              <ExportRow  printTableToPDF={() => {printTableToPDF()}} data={datas} />
-            </div>
+    <div className="container mx-auto mt-4 h-full" style={{ width: "95%" }}>
+      <div className=" flex justify-between">
+        <div>
+          <Breadcrumbs len={ids.length} ids={ids} />
+        </div>
+        <div className="flex justify-end space-x-2">
+          <div className="relative">
+            <FilterComponent props={'margin'} />
+          </div>
+          <div>
+            <ExportRow
+              printTableToPDF={() => {
+                printTableToPDF();
+              }}
+              data={datas}
+            />
+          </div>
         </div>
       </div>
-      <div className='overflow-auto'>
+      <div>
+        <FilteredData />
+      </div>
+      <div className="overflow-auto">
         <div id="table-to-print">
           <ReportsTable columns={columns} datas={datas} />
         </div>
       </div>
     </div>
-    )
-}
+  );
+};
 
 export default Margin;
