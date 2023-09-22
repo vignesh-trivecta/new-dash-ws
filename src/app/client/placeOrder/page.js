d132513a3a165ecdee6a53215b1aa568df1ab528
vignesh-trivecta <129928465+vignesh-trivecta@users.noreply.github.com>
2023-09-22 18:50:01 +0530

    Update page.js

--- src/app/client/placeOrder/page.js
+++ src/app/client/placeOrder/page.js
@@ -1,24 +1,26 @@
 'use client';
 
-import { Button, Spinner } from 'flowbite-react';
-import { useSearchParams } from 'next/navigation';
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
+import { useSearchParams } from 'next/navigation';
+import { Button, Spinner } from 'flowbite-react';
 
 const PlaceOrder = () => {
 
-    const basketData = useSelector((state) => state.client.basketData);
-
+    // url search params
     const searchParams = useSearchParams();
     const ssoId = searchParams.get('ssoId');
-
+    
+    // local state
     const [showSpinner, setShowSpinner] = useState(false);
     
+    // redux
+    const basketData = useSelector((state) => state.client.basketData);
 
     return (
         <div>
             {showSpinner
-                ?
+                ? // showing spinner when orders are being placed in the backend
                     (<div className='flex flex-col justify-center items-center h-screen'>
                         <Spinner
                             aria-label="Extra large spinner example"
@@ -27,7 +29,7 @@ const PlaceOrder = () => {
                         <p>Your request is being processed.</p>
                         <p> Don't leave or close this page.</p>
                     </div>)
-                :
+                :  // showing login failed when something breaks in backend and throws an error
                     (<div className='flex flex-col justify-center items-center h-screen space-y-10'>
                         <div className='flex flex-col items-center'>
                             <svg className="w-6 h-6 text-red-500 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
@@ -42,4 +44,4 @@ const PlaceOrder = () => {
     )
 }
 
-export default PlaceOrder
+export default PlaceOrder;
