39efaad5d785c3fe4317be5a096ea7f59cc686c0
vignesh-trivecta <129928465+vignesh-trivecta@users.noreply.github.com>
2023-09-22 18:54:00 +0530

    Update page.js

--- src/app/admin/reports/orderBook/page.js
+++ src/app/admin/reports/orderBook/page.js
@@ -1,118 +1,123 @@
-'use client';
+"use client";
 
-import React, { useState, useEffect } from 'react';
-import ExportRow from '@/components/page/exportRow';
-import FilterComponent from '@/components/page/filterComp';
+import React, { useState, useEffect } from "react";
+import ExportRow from "@/components/page/exportRow";
+import FilterComponent from "@/components/page/filterComp";
 // import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table';
 // import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css';
 // excel
 // import { DownloadTableExcel } from 'react-export-table-to-excel';
-import ReportsTable from '@/components/admin/reportsTable';
-import print from 'print-js';
-import { useSelector } from 'react-redux';
-import { handleFetchReports } from '@/app/api/reports/route';
-
+import ReportsTable from "@/components/admin/reportsTable";
+import { useSelector } from "react-redux";
+import { handleDbReportsFetch, handleLiveReportsFetch } from "@/app/api/reports/route";
+import FilteredData from "@/components/admin/filteredData";
+import Breadcrumbs from "@/components/page/breadcrumb";
+import print from "print-js";
+import orderDataParser from "@/utils/orderDataParser";
 
 const OrderBook = () => {
 
   // local state
-  const [data, setData] = useState([]);
+  const [tableData, setTableData] = useState([]);
+  const [tooltipData, setTooltipData] = useState([]);
+  // const [datas, setDatas] = useState([]);
+
+  // redux
+  const customerId = useSelector((state) => state.report.customerId);
+  const broker = useSelector((state) => state.report.broker);
+  const reportType = useSelector((state) => state.report.reportType);
+  const startDate = useSelector((state) => state.report.startDate);
+  const endDate = useSelector((state) => state.report.endDate);
+  const toggle = useSelector((state) => state.report.toggle);
 
   // const tableRef = useRef(null); // excel
 
+  // table to PDF
   const printTableToPDF = () => {
-    const tableId = 'table-to-print';
-    printJS({ printable: tableId, type: 'html',style: 'Td { border: 1px solid #D1D5DB !important;} Th { border: 1px solid #D1D5DB !important;}' });
+    const tableId = "table-to-print";
+    printJS({
+      printable: tableId,
+      type: "html",
+      style:
+        "Td { border: 1px solid #D1D5DB !important;} Th { border: 1px solid #D1D5DB !important;}",
+    });
   };
-  
 
-  const datas = [
-    {
-      "AtMarket": "N",
-      "BuySell": "B",
-      "Exch": "N",
-      "ExchOrderID": "1300000009880667",
-      "ExchOrderTime": "/Date(1658815566000+0530)/",
-      "ExchType": "C",
-      "OrderStatus": "Pending",
-      "Qty": 1,
-      "Rate": 12,
-      "ScripName": "YESBANK",
-      "RemoteOrderId": "1300000009880667",
-    },
-];
+  const ids = [{ Reports: "/admin/reports" }, { "Order Book": "" }];
 
-  const customerId = useSelector((state) => state.report.customerId);
-  const columns = Object.keys(datas[0]);
+  const columns = ["Remote Order ID", "Exchange", "Buy/ Sell", "Exchange Order Time", "Script", "Quantity", "Rate ₹", "Order Status", "Market/ Limit"];
 
+  // useEffect to fetch table data from backend
   useEffect(() => {
     const fetchOrderBook = async () => {
-      const response = await handleFetchReports("orderbook", customerId);
-      console.log(response);
-      // setData(response);
-    }
+      if (reportType === "Market") { // Live market data endpoint
+        console.log('market')
+        const response = await handleLiveReportsFetch(
+          "orderbook",
+          customerId,
+          startDate,
+          endDate
+        );
+        console.log(response);
+        // setDatas(response);
+
+        const { mainDatas, tooltipDatas} = orderDataParser(response);
+        setTableData(mainDatas);
+        setTooltipData(tooltipDatas);
+
+      }
+      else if (reportType === "Post") { // DB data endpoint
+        console.log('post')
+        const response = await handleDbReportsFetch(
+          "orderbook",
+          customerId,
+          startDate,
+          endDate
+        );
+
+        const { mainDatas, tooltipDatas} = orderDataParser(response);
+        setTableData(mainDatas);
+        setTooltipData(tooltipDatas);
+      }
+      else {
+        setTableData([])
+      }
+    };
     fetchOrderBook();
-  }, []);
-  
+  }, [toggle]);
+
   return (
-    <div className='container mx-auto mt-4 h-full' style={{width: '95%'}}>
-      <div className='flex justify-between'>
-        <h1 className="font-bold">Order Book</h1>
-        <div className='flex justify-end space-x-2'>
-            <div className='relative'>
-              <FilterComponent />
-            </div>
-            <div>
-              <ExportRow printTableToPDF={() => {printTableToPDF()}} data={datas} />
-            </div>
+    <div className="container mx-auto mt-4 h-full" style={{ width: "95%" }}>
+      <div className="flex justify-between">
+        <div>
+          <Breadcrumbs len={ids.length} ids={ids} />
+        </div>
+        <div className="flex justify-end space-x-2">
+          <div className="relative">
+            <FilterComponent props={'orderbook'} />
+          </div>
+          <div>
+            <ExportRow
+              printTableToPDF={() => {
+                printTableToPDF();
+              }}
+              data={tableData}
+            />
+          </div>
         </div>
       </div>
-      {/* <div className='overflow-auto' id="table-to-print" ref={tableRef}>
-        <ReportsTable columns={columns} datas={datas} />
-      </div> */}
-      {/* <div>
-            {/* <button> Export excel </button> 
-            <button onClick={convertTableToPDF}>Convert to PDF</button>
-            <button onClick={printTableToPDF}> 
-            </button> 
-        <DownloadTableExcel
-            filename="users table"
-            sheet="users"
-            currentTableRef={tableRef.current}
-        >
-
-
-        </DownloadTableExcel>
-      </div> 
-       <div className='overflow-y-scroll'>
-      <table className='table-fixed w-full border' >
-        <thead className='border bg-gray-50' >
-          <tr>
-          {
-            columns.map((heading, index) => {
-              return <th key={index} className='font-medium text-sm w-32 break-words border p-2'>{heading.split(/(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])/).join(' ')}</th>
-            })
-          }
-          </tr>
-        </thead>
-          <tbody>
-          {datas.map((data, index) => (
-          <tr className="border" key={index}>
-            {Object.values(data).map((value, subIndex) => (
-              <td className=" text-sm p-2 break-words border" key={subIndex}>{value}</td>
-            ))}
-          </tr>
-        ))}
-            </tbody>
-        </table>
-      </div> */}
-      <div className='overflow-auto'>
+      <div>
+        <FilteredData len={ids.length} ids={ids} />
+      </div>
+      <div className="overflow-auto">
         <div id="table-to-print">
-          <ReportsTable columns={columns} datas={datas} />
+          <ReportsTable columns={columns} datas={tableData} tooltipData={tooltipData} />
         </div>
       </div>
     </div>
-    )
-}
+  );
+};
 
 export default OrderBook;
+
+
