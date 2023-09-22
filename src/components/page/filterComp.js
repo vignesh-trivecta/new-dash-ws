88284530b28d61c7bc42ac71ba07f012ac619634
vignesh-trivecta <129928465+vignesh-trivecta@users.noreply.github.com>
2023-09-22 18:46:02 +0530

    Update filterComp.js

--- src/components/page/filterComp.js
+++ src/components/page/filterComp.js
@@ -1,17 +1,19 @@
 "use client";
 
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 import { Button, Dropdown, Label } from "flowbite-react";
 import { useDispatch, useSelector } from "react-redux";
-import { setBroker, setCustomerId, setDateType, setEndDate, setStartDate, setToggle } from "@/store/reportSlice";
+import { setBroker, setCustomerId, setDateType, setEndDate, setReportType, setStartDate, setToggle } from "@/store/reportSlice";
 import { getCustomers } from "@/app/api/basket/route";
 import { usePathname } from "next/navigation";
 import { BiFilterAlt } from "react-icons/bi";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { getBroker, handleDbReportsFetch, handleLiveReportsFetch } from "@/app/api/reports/route";
 
-const FilterComponent = () => {
+const FilterComponent = ({props, fetch, setFetch}) => {
 
+    // to get url pathname
     const pathName = usePathname();
     
     // redux state
@@ -19,25 +21,97 @@ const FilterComponent = () => {
     const customerId = useSelector((state) => state.report.customerId);
     const broker = useSelector((state) => state.report.broker);
     const dateType = useSelector((state) => state.report.dateType);
+    const reportType = useSelector((state) => state.report.reportType);
     const startDate = useSelector((state) => state.report.startDate);
     const endDate = useSelector((state) => state.report.endDate);
     const toggle = useSelector((state) => state.report.toggle);
+    const path = useSelector((state) => state.report.path);
     
     // local state
+    const [now, setNow] = useState(false);
     const [isOpen, setIsOpen] = useState(false);
+    const [localCustomerId, setLocalCustomerId] = useState(customerId);
     const [localBroker, setLocalBroker] = useState(broker);
+    const [localReportType, setLocalReportType] = useState('');
+    const [localDateType, setLocalDateType] = useState(dateType);
+    const [localStartDate, setLocalStartDate] = useState(startDate);
+    const [localEndDate, setLocalEndDate] = useState(endDate);
     const [list, setList] = useState([]);
-    // const [now, setNow] = useState(false);
+
+
+
+        // // finding current time and allowing Date radio button to be displayed
+        // // only when current IST time is greater than 3pm or 15:00 hrs
+        // const currentTime = new Date();
+        // const currentOffset = currentTime.getTimezoneOffset();
+        // const ISTOffset = 330;   // IST offset UTC +5:30 
+        // const ISTTime = new Date(currentTime.getTime() + (ISTOffset + currentOffset)*60000);
+
+        // // // IST Time now represents the time in IST coordinates
+        // const hoursIST = ISTTime.getHours()
+        // const minutesIST = ISTTime.getMinutes()
+        // console.log(hoursIST, minutesIST)
+        // if((hoursIST > 9) && (hoursIST <16)){
+        //     setNow(true);
+        // }
+        // else {
+        //     setNow(false);
+        // }
+
 
     // function to reset filter menu options
     const resetFilters = () => {
         setIsOpen(!isOpen);
-        dispatch(setCustomerId(list[0]));
-        setLocalBroker("AXIS");
-        dispatch(setBroker("AXIS"));
-        dispatch(setStartDate(new Date().setDate(new Date().getDate() - 1)));
-        dispatch(setEndDate(new Date().setDate(new Date().getDate() - 1)));
+        setLocalCustomerId("");
+        dispatch(setCustomerId(""));
+        setLocalBroker("");
+        dispatch(setBroker(""));
+        setLocalReportType('');
+        dispatch(setReportType(''));
+        setLocalStartDate(new Date(new Date().setDate(new Date().getDate() - 1)));
+        setLocalEndDate(new Date(new Date().setDate(new Date().getDate() - 1)));
+        dispatch(setStartDate(new Date(new Date().setDate(new Date().getDate() - 1))));
+        dispatch(setEndDate(new Date(new Date().setDate(new Date().getDate() - 1))));
         dispatch(setToggle(!toggle));
+    }
+
+    const handleFilter = async (e) => {
+        e.preventDefault();
+        console.log(startDate, endDate, localReportType)
+        dispatch(setToggle(!toggle));
+        dispatch(setCustomerId(localCustomerId));
+        dispatch(setBroker(localBroker));
+        dispatch(setReportType(localReportType));
+        dispatch(setDateType(localDateType));
+        dispatch(setStartDate(localStartDate));
+        dispatch(setEndDate(localEndDate));
+
+        if (pathName !== '/admin/reports') {
+            if (localReportType === 'Market') {
+                const response = await handleLiveReportsFetch(props ?? '', localCustomerId, localStartDate, localEndDate);
+                console.log(response)
+            }
+            else if (localReportType === 'Post') {
+                console.log(props ?? '', localCustomerId, localStartDate, localEndDate)
+                const response = handleDbReportsFetch(props ?? '', localCustomerId, localStartDate, localEndDate);
+                console.log(response);
+            }
+        }
+        else {
+            console.log("you are on reports page");
+        }
+        dispatch(setToggle(!toggle));
+    }
+
+    const handleCustomerSelection = async (e) => {
+        setLocalCustomerId(e.target.value);
+        const response = await getBroker((e.target.value).split(' ')[0]);
+        if (response) {
+            setLocalBroker(response);
+        }
+        else {
+            setLocalBroker("");
+        }
     }
 
     // useEffect to fetch data to show in table
@@ -53,27 +127,33 @@ const FilterComponent = () => {
 
         fetchData();
 
-        // finding current time and allowing Date radio button to be displayed
-        // only when current IST time is greater than 3pm or 15:00 hrs
-        // const currentTime = new Date();
-        // const currentOffset = currentTime.getTimezoneOffset();
-        // const ISTOffset = 330;   // IST offset UTC +5:30 
-        // const ISTTime = new Date(currentTime.getTime() + (ISTOffset + currentOffset)*60000);
-
-        // // IST Time now represents the time in IST coordinates
-        // const hoursIST = ISTTime.getHours()
-        // const minutesIST = ISTTime.getMinutes()
-        // if((hoursIST >= 15) && (minutesIST >= 0)){
-        //     setNow(true);
-        // }
-        // else {
-        //     setNow(false);
-        // }
 
     }, []);
 
+    useEffect(() => {
+        // finding current time and allowing Date radio button to be displayed
+        // only when current IST time is greater than 3pm or 15:00 hrs
+        const currentTime = new Date();
+        const currentOffset = currentTime.getTimezoneOffset();
+        const ISTOffset = 330;   // IST offset UTC +5:30 
+        const ISTTime = new Date(currentTime.getTime() + (ISTOffset + currentOffset)*60000);
+
+        // // IST Time now represents the time in IST coordinates
+        const hoursIST = ISTTime.getHours()
+        const minutesIST = ISTTime.getMinutes()
+        console.log(hoursIST, minutesIST, now)
+        if((hoursIST > 9) && (hoursIST <16)){
+            setNow(true);
+        }
+        else {
+            setNow(false);
+        }
+    })
+
     return (
         <div>
+            <form onSubmit={handleFilter}>
+            {/* Start of Filter dropdown */}
             <Dropdown
                 placement="bottom"
                 renderTrigger={() => (
@@ -91,9 +171,11 @@ const FilterComponent = () => {
                         <label className="font-medium text-sm">Customer Id</label>
                         <select 
                             className="rounded-md border-gray-200 text-sm"
-                            value={customerId}
-                            onChange={(e) => {dispatch(setCustomerId(e.target.value))}}
+                            value={localCustomerId}
+                            onChange={(e) => {handleCustomerSelection(e)}}
+                            required
                         >
+                            <option disabled value="">-Select -</option>
                             {
                                 list.map((id, index) => (
                                     <option 
@@ -110,19 +192,28 @@ const FilterComponent = () => {
                     {/* Broker */}
                     <div className="flex flex-col">
                         <label className="font-medium text-sm">Broker</label>
-                        <select value={localBroker} onChange={(e) => {setLocalBroker(e.target.value); dispatch(setBroker(e.target.value))}} className="border border-gray-200 rounded-md w-full text-sm" name="broker" id="broker" >
+                        <select value={localBroker} required onChange={(e) => {setLocalBroker(e.target.value)}} className="border border-gray-200 rounded-md w-full text-sm" name="broker" id="broker" >
+                            <option disabled value="">-Select -</option>
                             <option value="AXIS">AXIS</option>
                             <option value="IIFL">IIFL</option>
+                        </select> 
+                    </div>
+
+                    {/* Report Type */}
+                    <div className="flex flex-col">
+                        <label className="font-medium text-sm">Report Type</label>
+                        <select value={localReportType} required onChange={(e) => {setLocalReportType(e.target.value)}} className="border border-gray-200 rounded-md w-full text-sm" name="broker" id="broker" >
+                            <option disabled value="">-Select -</option>
+                            <option value="Market">Market Hours</option>
+                            <option value="Post">Post Market</option>
                         </select> 
                     </div>
                     
                     {/* Dates radio button */}
                     <div>
                         {
-                        // now 
+                        // (now && pathName === "/admin/reports/ledger" ) 
                         // && 
-                        pathName === "/admin/reports/ledger"
-                        && 
                         <>
                             <Label value="Date" className="col-start-1 row-start-2 text-sm" />
                             <div className=" col-start-2 row-start-2">
@@ -132,9 +223,9 @@ const FilterComponent = () => {
                                     name="date" 
                                     type="radio" 
                                     value="today"
-                                    defaultChecked={dateType === "today"}
+                                    defaultChecked={localDateType === "today"}
                                     onClick={() => {
-                                        dispatch(setDateType("today"));
+                                        setLocalDateType("today");
                                 }} />
                                 <label htmlFor="today" className="ml-1 text-sm">Today</label>
                                 <input 
@@ -144,30 +235,31 @@ const FilterComponent = () => {
                                     type="radio" 
                                     value="custom" 
                                     className="ml-1" 
-                                    defaultChecked={dateType === "custom"}
+                                    defaultChecked={localDateType === "custom"}
                                     onClick={() => {
-                                        dispatch(setDateType("custom"));
+                                        setLocalDateType("custom");
                                 }} />
                                 <label htmlFor="custom" className="ml-1 text-sm">Custom</label>
                             </div>
                         </>
                         }
                     </div>
- 
-                    {dateType === "custom" && 
+                    
+                    {/* show this date pickers if datetype is custom */}
+                    {localDateType === "custom" && 
                     <div>
 
                         <div className="flex flex-col">
                             <label className="font-medium text-sm">From</label>
                             <DatePicker
                                 dateFormat="dd-MMM-yyyy"
-                                selected={startDate}
-                                onChange={(date) => dispatch(setStartDate(date))}
+                                selected={localStartDate}
+                                onChange={(date) => setLocalStartDate(date)}
                                 selectsStart
-                                startDate={startDate}
-                                endDate={endDate}
+                                startDate={localStartDate}
+                                endDate={localEndDate}
                                 minDate={new Date("1995-12-17T03:24:00")}
-                                maxDate={startDate}
+                                maxDate={localEndDate}
                                 className="text-sm border-gray-200 rounded-md z-2"
                             /> 
                         </div>                       
@@ -175,13 +267,13 @@ const FilterComponent = () => {
                             <label className="font-medium text-sm">To</label>
                             <DatePicker
                                 dateFormat="dd-MMM-yyyy"
-                                selected={endDate}
-                                onChange={(date) => dispatch(setEndDate(date))}
+                                selected={localEndDate}
+                                onChange={(date) => setLocalEndDate(date)}
                                 selectsEnd
-                                startDate={startDate}
-                                endDate={endDate}
-                                minDate={startDate}
-                                maxDate={new Date().setDate(new Date().getDate() - 1)}
+                                startDate={localStartDate}
+                                endDate={localEndDate}
+                                minDate={localStartDate}
+                                maxDate={new Date(new Date().setDate(new Date().getDate() - 1))}
                                 className="text-sm border-gray-200 rounded-md"
                             />                        
                         </div>
@@ -191,11 +283,12 @@ const FilterComponent = () => {
 
                 {/* Buttons group */}
                 <div className="mt-4 flex space-x-2 justify-center"> 
-                    <Button
-                        size={"sm"}
+                    <button
+                        type="submit"
+                        className="text-sm bg-cyan-800 hover:bg-cyan-700 border p-1 rounded-md text-white w-16"
                     >
                         Filter
-                    </Button>
+                    </button>
                     <Button
                         size={"sm"}
                         color={"gray"}
@@ -205,6 +298,7 @@ const FilterComponent = () => {
                     </Button>
                 </div>
             </Dropdown> 
+            </form>
         </div>
     )
 }
