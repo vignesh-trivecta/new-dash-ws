a254afcf52b0a3d638c349e3ba347ddbb23fcd75
vignesh-trivecta <129928465+vignesh-trivecta@users.noreply.github.com>
2023-09-27 14:30:15 +0530

    Update page.js

--- src/app/admin/reports/orderBook/page.js
+++ src/app/admin/reports/orderBook/page.js
@@ -1,18 +1,13 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import ExportRow from "@/components/page/exportRow";
-import FilterComponent from "@/components/page/filterComp";
-// import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table';
-// import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css';
-// excel
-// import { DownloadTableExcel } from 'react-export-table-to-excel';
-import ReportsTable from "@/components/admin/reportsTable";
 import { useSelector } from "react-redux";
 import { handleDbReportsFetch, handleLiveReportsFetch } from "@/app/api/reports/route";
+import ReportsTable from "@/components/admin/reportsTable";
 import FilteredData from "@/components/admin/filteredData";
+import ExportRow from "@/components/page/exportRow";
+import FilterComponent from "@/components/page/filterComp";
 import Breadcrumbs from "@/components/page/breadcrumb";
-import print from "print-js";
 import orderDataParser from "@/utils/orderDataParser";
 
 const OrderBook = () => {
@@ -24,28 +19,17 @@ const OrderBook = () => {
 
   // redux
   const customerId = useSelector((state) => state.report.customerId);
-  const broker = useSelector((state) => state.report.broker);
   const reportType = useSelector((state) => state.report.reportType);
   const startDate = useSelector((state) => state.report.startDate);
   const endDate = useSelector((state) => state.report.endDate);
   const toggle = useSelector((state) => state.report.toggle);
 
-  // const tableRef = useRef(null); // excel
-
-  // table to PDF
-  const printTableToPDF = () => {
-    const tableId = "table-to-print";
-    printJS({
-      printable: tableId,
-      type: "html",
-      style:
-        "Td { border: 1px solid #D1D5DB !important;} Th { border: 1px solid #D1D5DB !important;}",
-    });
-  };
-
+  // Data for breadcrumb
   const ids = [{ Reports: "/admin/reports" }, { "Order Book": "" }];
 
-  const columns = ["Remote Order ID", "Exchange", "Buy/ Sell", "Exchange Order Time", "Script", "Quantity", "Rate ₹", "Order Status", "Market/ Limit"];
+  // column/header data for excel and pdf
+  const excelColumns = ["Remote Order ID", "Exchange", "Buy/ Sell", "Exchange Order Time", "Script", "Quantity", "Rate ₹", "Order Status", "Market/ Limit"];
+  const pdfColumns = ["Remote Order ID", "Exchange", "Buy/ Sell", "Exchange Order Time", "Script", "Quantity", "Rate", "Order Status", "Market/ Limit"];
 
   // useEffect to fetch table data from backend
   useEffect(() => {
@@ -62,23 +46,22 @@ const OrderBook = () => {
         // setDatas(response);
 
         const { mainDatas, tooltipDatas} = orderDataParser(response);
-        console.log(tooltipDatas)
         setTableData(mainDatas);
         setTooltipData(tooltipDatas);
 
       }
       else if (reportType === "Post") { // DB data endpoint
-        console.log('post')
         const response = await handleDbReportsFetch(
           "orderbook",
           customerId,
           startDate,
           endDate
         );
-
-        const { mainDatas, tooltipDatas} = orderDataParser(response);
-        setTableData(mainDatas);
-        setTooltipData(tooltipDatas);
+        if (response !== 404 || response !== "Failed to fetch") {
+          const { mainDatas, tooltipDatas} = orderDataParser(response);
+          setTableData(mainDatas);
+          setTooltipData(tooltipDatas);
+        }
       }
       else {
         setTableData([])
@@ -97,24 +80,22 @@ const OrderBook = () => {
           />
         </div>
         <div className="flex justify-end space-x-2">
+          <div>
+            <ExportRow
+              data={tableData}
+              columns={excelColumns}
+              pdfColumns={pdfColumns}
+              fileName={'ledger'}
+            />
+          </div>
           <div className="relative">
             <FilterComponent 
               props={'orderbook'} 
             />
           </div>
-          <div>
-            <ExportRow
-              printTableToPDF={() => {
-                printTableToPDF();
-              }}
-              data={tableData}
-              columns={columns}
-              fileName={'orderBook'}
-            />
-          </div>
         </div>
       </div>
-      <div id="table-to-print">
+      <div>
         <div>
           <FilteredData 
             len={ids.length} 
@@ -124,13 +105,12 @@ const OrderBook = () => {
         <div className="overflow-auto">
           <div>
             <ReportsTable 
-              columns={columns} 
+              columns={excelColumns} 
               datas={tableData} 
               tooltipData={tooltipData} 
             />
           </div>
         </div>
-
       </div>
     </div>
   );
