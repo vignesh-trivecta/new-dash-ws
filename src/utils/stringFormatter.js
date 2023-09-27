27745b8b2fc7265adc2dd355ae08b3fe545ed56f
vignesh-trivecta <129928465+vignesh-trivecta@users.noreply.github.com>
2023-09-27 14:32:55 +0530

    Update stringFormatter.js

--- src/utils/stringFormatter.js
+++ src/utils/stringFormatter.js
@@ -1,15 +1,15 @@
 import formatDate from "./format-date";
 
 const stringFormatter = (str) => {
-    if (typeof str === 'string' && str.includes('-') && str.split('-').length == 3) {
-        if (str.includes('T')) {
-            const date = str.split('T');
-            return (formatDate(date[0]) + " | " + new Date (str).toLocaleTimeString());
+    if (typeof str === 'string' && str?.includes('-') && str?.split('-').length == 3 && str) {
+        if (str?.includes('T')) {
+            const date = str?.split('T');
+            return (formatDate(date[0]) + " | " + new Date (str)?.toLocaleTimeString());
         }
         return formatDate(str);
     }
-    if (str.length > 50) {
-        return str.slice(0,50);
+    if (str?.length > 50) {
+        return str?.slice(0,50);
     }
     return str;
 }
