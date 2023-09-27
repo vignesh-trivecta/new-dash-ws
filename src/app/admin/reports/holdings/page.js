73376e5bcc6b8a23ad71a5c567406ee2281eb774
vignesh-trivecta <129928465+vignesh-trivecta@users.noreply.github.com>
2023-09-27 14:29:27 +0530

    Update page.js

--- src/app/admin/reports/holdings/page.js
+++ src/app/admin/reports/holdings/page.js
@@ -1,16 +1,13 @@
 "use client";
 
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { handleDbReportsFetch, handleLiveReportsFetch } from "@/app/api/reports/route";
-import ExportRow from "@/components/page/exportRow";
-import FilterComponent from "@/components/page/filterComp";
 import ReportsTable from "@/components/admin/reportsTable";
 import FilteredData from "@/components/admin/filteredData";
-import print from "print-js";
+import ExportRow from "@/components/page/exportRow";
+import FilterComponent from "@/components/page/filterComp";
 import Breadcrumbs from "@/components/page/breadcrumb";
-import { usePathname } from "next/navigation";
-import { setPath } from "@/store/reportSlice";
 
 const Holdings = () => {
 
@@ -19,35 +16,21 @@ const Holdings = () => {
 
   // redux
   const customerId = useSelector((state) => state.report.customerId);
-  const broker = useSelector((state) => state.report.broker);
   const reportType = useSelector((state) => state.report.reportType);
   const startDate = useSelector((state) => state.report.startDate);
   const endDate = useSelector((state) => state.report.endDate);
   const toggle = useSelector((state) => state.report.toggle);
-
-  console.log(startDate, endDate)
-
-  // table to PDF
-  const printTableToPDF = () => {
-    const tableId = "table-to-print";
-    printJS({
-      printable: tableId,
-      type: "html",
-      style:
-        "Td { border: 1px solid #D1D5DB !important;} Th { border: 1px solid #D1D5DB !important;}",
-    });
-  };
-
   
+  // Data for breadcrumb
   const ids = [{ Reports: "/admin/reports" }, { Holdings: "" }];
 
-  const columns = ["Script", "Exchange", "Quantity", "Current Price ₹", "Current Value ₹", "% Change"]
+  // column/header data for excel and pdf
+  const columns = ["Script", "Exchange", "Quantity", "Current Price", "Current Value", "% Change"]
 
   // useEffect to fetch table data from backend
   useEffect(() => {
     const fetchHoldings = async () => {
       if (reportType === "Market") { // Live market data endpoint
-        console.log('market')
         const response = await handleLiveReportsFetch(
           "holdings",
           customerId,
@@ -58,14 +41,12 @@ const Holdings = () => {
         // setData(response);
       }
       else if (reportType === "Post") { // DB data endpoint
-        console.log('post')
         const response = await handleDbReportsFetch(
           "holdings",
           customerId,
           startDate,
           endDate
         );
-        console.log(response);
         setTableData(response);
       }
       else {
@@ -79,28 +60,38 @@ const Holdings = () => {
     <div className="container mx-auto mt-4 h-full" style={{ width: "95%" }}>
       <div className="flex justify-between">
         <div>
-          <Breadcrumbs len={2} ids={ids} />
+          <Breadcrumbs 
+            len={2} 
+            ids={ids} 
+          />
         </div>
         <div className="flex justify-end space-x-2">
-          <div className="">
-            <FilterComponent props={'holdings'} />
-          </div>
           <div>
             <ExportRow
-              printTableToPDF={() => {
-                printTableToPDF();
-              }}
               data={tableData}
+              columns={columns}
+              pdfColumns={columns}
+              fileName={'holdings'}
+            />
+          </div>
+          <div className="">
+            <FilterComponent 
+              props={'holdings'} 
             />
           </div>
         </div>
       </div>
-      <div>
-        <FilteredData />
-      </div>
-      <div className="overflow-auto">
-        <div id="table-to-print">
-          <ReportsTable columns={columns} datas={tableData} />
+      <div id="table-to-print">
+        <div>
+          <FilteredData />
+        </div>
+        <div className="overflow-auto">
+          <div >
+            <ReportsTable 
+              columns={columns} 
+              datas={tableData} 
+            />
+          </div>
         </div>
       </div>
     </div>
