5ca0dfaff6dfac93f7573b64f87122a872ed118c
vignesh-trivecta <129928465+vignesh-trivecta@users.noreply.github.com>
2023-09-28 18:22:29 +0530

    Update reportsList.js

--- src/components/page/reportsList.js
+++ src/components/page/reportsList.js
@@ -8,8 +8,8 @@ const ReportsList = () => {
     <div>
         <ul className='space-y-4 list-disc ml-4 mt-8'>
             <li><Link href={'/admin/reports/holdings'} className='underline text-blue-700'>Holdings</Link></li>
-            <li><Link href={'/admin/reports/orderBook'} className='underline text-blue-700'>Order Book</Link></li>
-            <li><Link href={'/admin/reports/tradeBook'} className='underline text-blue-700'>Trade Book</Link></li>
+            <li><Link href={'/admin/reports/orderbook'} className='underline text-blue-700'>Order Book</Link></li>
+            <li><Link href={'/admin/reports/tradebook'} className='underline text-blue-700'>Trade Book</Link></li>
             <li><Link href={'/admin/reports/margin'} className='underline text-blue-700'>Margin</Link></li>
             <li><Link href={'/admin/reports/ledger'} className='underline text-blue-700'>Ledger</Link></li>
         </ul>
