a2d0caa993607ce06a8f519e49a495938715650d
vignesh-trivecta <129928465+vignesh-trivecta@users.noreply.github.com>
2023-09-28 18:19:00 +0530

    Update stringFormatter.js

--- src/utils/stringFormatter.js
+++ src/utils/stringFormatter.js
@@ -1,15 +1,31 @@
 import formatDate from "./format-date";
 
-const stringFormatter = (str) => {
-    if (typeof str === 'string' && str?.includes('-') && str?.split('-').length == 3 && str) {
-        if (str?.includes('T')) {
-            const date = str?.split('T');
-            return (formatDate(date[0]) + " | " + new Date (str)?.toLocaleTimeString());
+const stringFormatter = (str, time) => {
+    if (typeof str === 'string' && str?.includes('-') && str?.split('-').length === 3 && str) {
+        if (str?.includes(' ') || str?.includes('T')) {
+            const date = str?.split(' ') || str?.split('T');
+            const dateTime = new Date(str);
+            let formattedTime;
+
+            if (time === 12) {
+                formattedTime = dateTime.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
+            } else {
+                formattedTime = dateTime.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false });
+            }
+
+            if (time !== 12) {
+                formattedTime = formattedTime.slice(0, -3); // Remove the last colon and seconds
+            }
+            return (
+                formatDate(date[0]) 
+                + " | " 
+                + formattedTime
+            );
         }
         return formatDate(str);
     }
     if (str?.length > 50) {
-        return str?.slice(0,50);
+        return str?.slice(0, 50);
     }
     return str;
 }
