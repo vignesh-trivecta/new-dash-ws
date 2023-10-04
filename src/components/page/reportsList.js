95e4df6abd4961826f816192ac8fc8491b0cd75b
vignesh-trivecta <129928465+vignesh-trivecta@users.noreply.github.com>
2023-10-04 16:27:53 +0530

    Update reportsList.js

--- src/components/page/reportsList.js
+++ src/components/page/reportsList.js
@@ -7,7 +7,7 @@ const ReportsList = () => {
   return (
     <div>
         <ul className='space-y-4 list-disc ml-4 mt-8'>
-            <li><Link href={'/admin/reports/holdings'} className='underline text-blue-700'>Holdings</Link></li>
+            <li><Link href={'/admin/reports/holding'} className='underline text-blue-700'>Holding</Link></li>
             <li><Link href={'/admin/reports/orderbook'} className='underline text-blue-700'>Order Book</Link></li>
             <li><Link href={'/admin/reports/tradebook'} className='underline text-blue-700'>Trade Book</Link></li>
             <li><Link href={'/admin/reports/margin'} className='underline text-blue-700'>Margin</Link></li>
@@ -17,4 +17,4 @@ const ReportsList = () => {
   )
 }
 
-export default ReportsList
+export default ReportsList;
