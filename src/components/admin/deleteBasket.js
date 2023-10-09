70949609330e4ad2bd9e51c9ddcd9fed1be9ecb5
vignesh-trivecta <129928465+vignesh-trivecta@users.noreply.github.com>
2023-10-09 17:39:08 +0530

    Update deleteBasket.js

--- src/components/admin/deleteBasket.js
+++ src/components/admin/deleteBasket.js
@@ -1,26 +1,41 @@
-import { deleteBasket } from '@/app/api/mainBasket/route';
-import Link from 'next/link'
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import { deleteRecordMainAPI } from '@/app/api/mainBasket/route';
+import { deleteRecord } from '@/app/api/tempBasket/route';
 import { useSelector } from 'react-redux';
+import { usePathname } from 'next/navigation';
 
-const DeleteBasket = ({ handleFetch, setHandleFetch, basketName }) => {
+const DeleteRecord = ({ recId, mainBasketName, handleFetch, setHandleFetch }) => {
 
+    let pathname = usePathname();
+
+    const basketName = useSelector((state) => state.basket.basketName);
     const adminId = useSelector((state) => state.user.username);
 
     const handleDelete = async() => {
-        const response = await deleteBasket( basketName, adminId );
-        console.log(response);
-        setHandleFetch(!handleFetch);
+        if((pathname == `/admin/baskets/view/${mainBasketName}/update`) && (mainBasketName.includes("%20"))){
+            const response = await deleteRecordMainAPI(recId, mainBasketName, adminId );
+            setHandleFetch(!handleFetch);           
+        }
+        else if(pathname == `/admin/baskets/view/${mainBasketName}/update`){
+            const response = await deleteRecordMainAPI(recId, mainBasketName, adminId );
+            setHandleFetch(!handleFetch);           
+        }
+        else{
+            const response = await deleteRecord(recId, basketName, adminId );
+            setHandleFetch(!handleFetch);
+        }
     }
+
   return (
     <div>
         <Link href="#" onClick={handleDelete}>
-            <svg className="w-4 h-4 text-gray-500 hover:text-red-500 hover:fill-red-500 dark:text-white" ariaHidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 18">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M1 5h16M7 8v8m4-8v8M7 1h4a1 1 0 0 1 1 1v3H6V2a1 1 0 0 1 1-1ZM3 5h12v13a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V5Z"/>
+            <svg className="w-4 h-4 text-gray-500 hover:text-red-500 dark:text-white" ariaHidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h16M7 8v8m4-8v8M7 1h4a1 1 0 0 1 1 1v3H6V2a1 1 0 0 1 1-1ZM3 5h12v13a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V5Z"/>
             </svg>
         </Link>
     </div>
   )
 }
 
-export default DeleteBasket;
+export default DeleteRecord
