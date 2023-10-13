171fe3354a1bba427d0dabe2710ffe1d63e05620
vignesh <vignesh.murugan@trivectadigital.com>
2023-10-13 19:03:35 +0530

    commit

--- src/components/admin/table/mappingTable.js
+++ src/components/admin/table/mappingTable.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Tooltip } from 'flowbite-react';
 import { getCustomerStatus, mapBasket } from '@/app/api/basket/route';
 import { useSelector } from 'react-redux';
+import { segregate } from '@/utils/formatter/priceSegregator';
 
-const MappingTable = ({ data, index, status, setStatus }) => {
+const MappingTable = ({ data, index, status, setStatus, enableInputs, basketName, basketVal }) => {
 
     // broker inputs 
     const brokers = [
@@ -13,14 +14,20 @@ const MappingTable = ({ data, index, status, setStatus }) => {
 
     // redux
     const adminId = useSelector((state) => state.user.username);
-    const basketName = useSelector((state) => state.basket.basketName);
+    console.log(basketName, basketName == "")
 
     // local state
     const [broker, setBroker] = useState(brokers[0].name);
+    const [investment, setInvestment] = useState('');
+    const [quantity, setQuantity] = useState('');
+    const [total, setTotal] = useState('');
+    const [highlight, setHighlight] = useState(false);
+    const [enableButtons, setEnableButtons] = useState(basketName == "");
 
     // handle customer mapping
     const handleMapping = async (customerId) => {
-        const response = await mapBasket(basketName, adminId, customerId, broker);
+        console.log('clicked')
+        const response = await mapBasket(basketName, adminId, customerId, broker, basketVal);
         const status = await getCustomerStatus(basketName);
         if(status){
             setStatus(status);
@@ -32,6 +39,25 @@ const MappingTable = ({ data, index, status, setStatus }) => {
         
     }
 
+    useEffect(() => {
+        if ( Number(investment) !== 0 && Number(quantity) !== 0) {
+            setEnableButtons(false);
+        }
+        else {
+            setEnableButtons(true);
+        }
+
+        if (Number(total) > Number(basketVal)) {
+            console.log(total, basketVal, highlight)
+            setHighlight(true);
+            setEnableButtons(true);
+
+        }
+        else {
+            setHighlight(false);
+        }
+    }, [investment, quantity])
+
     return (
         <tr key={index} className='border-b p-2 hover:bg-gray-100'>
             <td className="text-sm text-left text-black p-2 break-words">
@@ -40,17 +66,58 @@ const MappingTable = ({ data, index, status, setStatus }) => {
             <td className="text-sm text-left text-black break-words">
                 {data.name}
             </td>
-            <td className="text-sm text-left text-black break-words">
+            {/* <td className="text-sm text-left text-black break-words">
                 {data.email}
-            </td>
-            <td className="text-sm text-center text-black break-words">
-                {data.contactOne}
-            </td>
+            </td> */}
             <td className="text-sm text-center text-black">
                 <select className="text-xs border-gray-200 rounded-md" value={broker} onChange={e => setBroker(e.target.value)} >
                     <option className="text-sm" value={'AXIS'}>AXIS</option>
                     <option className="text-sm" value={'IIFL'}>IIFL</option>
                 </select>
+            </td>
+            <td className=" text-sm text-black">
+                <input 
+                    id='investment'
+                    value={segregate(investment)}
+                    disabled={enableInputs}
+                    onChange={(e) => {
+                        // Remove commas from the input value before updating state
+                        const newValue = e.target.value.replace(/,/g, "");
+                        setInvestment(newValue);
+                        setTotal(newValue * quantity);
+                    }}
+                    type='text' 
+                    className={`w-28 h-8 text-right border-gray-300 rounded-md`}
+                />
+            </td>
+            <td className=" text-sm text-black">
+                <input 
+                    id='quantity'
+                    value={segregate(quantity)}
+                    disabled={enableInputs}
+                    onChange={(e) => {
+                        // Remove commas from the input value before updating state
+                        const newValue = e.target.value.replace(/,/g, "");
+                        setQuantity(newValue);
+                        setTotal(investment * newValue);
+                    }}
+                    type='text' 
+                    className={`ml-2 w-24 h-8 text-right border-gray-300 rounded-md`}
+                />
+            </td>
+            <td className=" text-sm text-black">
+                <input 
+                    disabled
+                    id='total'
+                    value={segregate(total)}
+                    onChange={(e) => {
+                        // Remove commas from the input value before updating state
+                        const newValue = e.target.value.replace(/,/g, "");
+                        setTotal(newValue)
+                    }}
+                    type='text' 
+                    className={`w-28 h-8 text-right border-gray-300 bg-gray-100 rounded-md ${highlight ? 'border-red-500' : 'border-gray-300'}`}
+                />
             </td>
             {/* Map status */}
             <td>
@@ -79,7 +146,7 @@ const MappingTable = ({ data, index, status, setStatus }) => {
             <td className=" flex text-sm text-black mt-2 md: lg:ml-4 ">
                 {/* Map customer */}
                 <Tooltip content="Map Customer">
-                    <button className="ml-2" onClick={() => {handleMapping(data.customerId)}}>
+                    <button disabled={enableButtons} className="ml-2" onClick={() => {handleMapping(data.customerId)}}>
                     <svg className="w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 18">
                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="m1 14 3-3m-3 3 3 3m-3-3h16v-3m2-7-3 3m3-3-3-3m3 3H3v3"/>
                     </svg>
@@ -87,7 +154,7 @@ const MappingTable = ({ data, index, status, setStatus }) => {
                 </Tooltip>
                 {/* Send Weblink */}
                 <Tooltip content="Send Weblink">
-                    <button className="ml-4">
+                    <button disabled={enableButtons} className="ml-4">
                         <svg className="w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 18">
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M5.5 6.5h.01m4.49 0h.01m4.49 0h.01M18 1H2a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h3v5l5-5h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z"/>
                         </svg>
