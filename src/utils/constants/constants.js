182f4d08985e55cc6573f4cb10562f2b036ee0e7
vignesh-trivecta <129928465+vignesh-trivecta@users.noreply.github.com>
2023-11-24 15:14:38 +0530

    Update constants.js

--- src/utils/constants/constants.js
+++ src/utils/constants/constants.js
@@ -1,4 +1,6 @@
 export const holdingColumns = ["Script", "Script Type", "Quantity", "Current Value ₹", "Date"];
+export const holdingExcelColumns = ["Script", "Script Type", "Quantity", "Current Value ₹", "Date"];
+export const holdingPdfColumns = ["Script", "Script Type", "Quantity", "Current Value ₹", "Date"];
 export const ledgerExcelColumns = ["Voucher Date", "Voucher Number", "Narration", "Debit/ Credit", "Amount ₹"];
 export const ledgerPdfColumns = ["Voucher Date", "Voucher Number", "Narration", "Debit/ Credit", "Amount"];
 export const marginExcelColumns = ["Adjusted Ledger Balance ₹", "Gross Holding Value ₹", "Gross Margin ₹", "Available Margin ₹"];
