2f124d2e61b4975e646c44e96cf08824c1907b49
vignesh-trivecta <129928465+vignesh-trivecta@users.noreply.github.com>
2023-11-28 11:07:21 +0530

    Update tableShimmer.js

--- src/components/page/layout/tableShimmer.js
+++ src/components/page/layout/tableShimmer.js
@@ -3,7 +3,7 @@ import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table';
 
 const TableShimmer = ({datas}) => {
   return (
-    <div className="border" style={{ height: '380px' }}>
+    <div className="border h-[calc(100vh-300px)]">
         <Table className="w-full">
             <Thead className=" h-10 bg-gray-50">
                 <Tr>
@@ -21,9 +21,23 @@ const TableShimmer = ({datas}) => {
                     </Th>
                 </Tr>
             </Thead>
-            <Tbody className="border h-10">
+            <Tbody className="border">
                 <Tr>
                     <Td>
+                        <p className='h-[calc(100vh-345px)] p-2 bg-gray-100'>&nbsp;</p>
+                    </Td>
+                    <Td>
+                        <p className='h-[calc(100vh-345px)] p-2 bg-gray-100'>&nbsp;</p>
+                    </Td>
+                    <Td>
+                        <p className='h-[calc(100vh-345px)] p-2 bg-gray-100'>&nbsp;</p>
+                    </Td>
+                    <Td>
+                        <p className='h-[calc(100vh-345px)] p-2 bg-gray-100'>&nbsp;</p>
+                    </Td>
+                </Tr>
+                {/* <Tr className="border h-10">
+                    <Td>
                         <p className='p-2 bg-gray-100'>&nbsp;</p>
                     </Td>
                     <Td>
@@ -78,6 +92,34 @@ const TableShimmer = ({datas}) => {
                         <p className='p-2 bg-gray-100'>&nbsp;</p>
                     </Td>
                 </Tr>
+                <Tr className="border h-10">
+                    <Td>
+                        <p className='p-2 bg-gray-100'>&nbsp;</p>
+                    </Td>
+                    <Td>
+                        <p className='p-2 bg-gray-100'>&nbsp;</p>
+                    </Td>
+                    <Td>
+                        <p className='p-2 bg-gray-100'>&nbsp;</p>
+                    </Td>
+                    <Td>
+                        <p className='p-2 bg-gray-100'>&nbsp;</p>
+                    </Td>
+                </Tr>
+                <Tr className="border h-10">
+                    <Td>
+                        <p className='p-2 bg-gray-100'>&nbsp;</p>
+                    </Td>
+                    <Td>
+                        <p className='p-2 bg-gray-100'>&nbsp;</p>
+                    </Td>
+                    <Td>
+                        <p className='p-2 bg-gray-100'>&nbsp;</p>
+                    </Td>
+                    <Td>
+                        <p className='p-2 bg-gray-100'>&nbsp;</p>
+                    </Td>
+                </Tr> */}
             </Tbody>
         </Table>
     </div>
