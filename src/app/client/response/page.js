56a705530060412f648694861271f86420ff0c55
vignesh-trivecta <129928465+vignesh-trivecta@users.noreply.github.com>
2023-12-01 18:39:25 +0530

    Create page.js

---
+++ src/app/client/response/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CustomerResponse = () => {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+        <p className="text-lg text-center text-green-700">Thank you!</p>
+        {/* <p className="text-lg text-center text-green-700">Your response has been saved.</p> */}
+        <p className="text-lg text-center underline">You can exit the page</p>
+    </div>
+  )
+}
+
+export default CustomerResponse
