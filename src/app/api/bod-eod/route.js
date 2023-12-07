b73b78ac02b501d13ff03598d5cbfeba6b41cb09
vignesh-trivecta <vignesh.murugan@trivectadigital.com>
2023-12-07 10:27:25 +0530

    update: shedule tasks api call endpoint added

---
+++ src/app/api/bod-eod/route.js
@@ -0,0 +1,21 @@
+// bod- eod => Schedule tasks call
+export const executeScheduleTasks = async () => {
+    console.log("enter")
+    try {
+        console.log("enter")
+        const requestOptions = {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        }
+        const response = await fetch("http://13.200.85.83:8085/execute", requestOptions);
+
+        const res = await response.text();
+        const status = response.status
+        return {res, status};
+
+    } catch (error) {
+        return {res: "Error executing scheduled tasks", status: 500};
+    }
+}
