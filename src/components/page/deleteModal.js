54bde449214fad65ec8ba7152652f1e280a58931
vignesh-trivecta <vignesh.murugan@trivectadigital.com>
2024-01-08 10:46:34 +0530

    created Delete component and refactored old codes

---
+++ src/components/page/deleteModal.js
@@ -0,0 +1,64 @@
+"use client";
+
+import { Button, Modal } from "flowbite-react";
+import { useState } from "react";
+import { HiOutlineExclamationCircle } from "react-icons/hi";
+
+export function DeleteModal(props) {
+  const [openModal, setOpenModal] = useState(false);
+
+  return (
+    <>
+      {props.type === "svg" ? (
+        <svg
+          onClick={() => setOpenModal(true)}
+          className="hover:cursor-pointer w-4 h-4 text-gray-500 hover:text-red-500 dark:text-white"
+          ariaHidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 18 20"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M1 5h16M7 8v8m4-8v8M7 1h4a1 1 0 0 1 1 1v3H6V2a1 1 0 0 1 1-1ZM3 5h12v13a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V5Z"
+          />
+        </svg>
+      ) : (
+        <Button onClick={() => setOpenModal(true)}>{props.name}</Button>
+      )}
+      <Modal
+        show={openModal}
+        size="sm"
+        onClose={() => setOpenModal(false)}
+        popup
+      >
+        <Modal.Header />
+        <Modal.Body>
+          <div className="text-center">
+            <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
+            <h3 className="mb-5 text-sm font-normal text-gray-500 dark:text-gray-400">
+              Are you sure you want to delete this item?
+            </h3>
+            <div className="flex justify-center gap-4">
+              <Button
+                color="failure"
+                onClick={() => {
+                  props.deleteFunction();
+                  setOpenModal(false);
+                }}
+              >
+                {"Yes, I'm sure"}
+              </Button>
+              <Button color="gray" onClick={() => setOpenModal(false)}>
+                No, cancel
+              </Button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+}
