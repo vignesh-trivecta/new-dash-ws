26a0a9c10e1778e865d1728b32b639c64f38a7d0
vignesh-trivecta <vignesh.murugan@trivectadigital.com>
2024-01-17 10:08:13 +0530

    enc-dec done on customer mapping page

--- src/app/api/map/baskets/route.js
+++ src/app/api/map/baskets/route.js
@@ -1,3 +1,7 @@
+import { decrypt } from "@/utils/aesDecryptor";
+import { encrypt } from "@/utils/aesEncryptor";
+import { errorLogger } from "@/utils/errorLogger";
+
 const DOMAIN = process.env.NEXT_PUBLIC_DOMAIN;
 const PORT = process.env.NEXT_PUBLIC_CORE_COMP_PORT;
 
@@ -9,26 +13,23 @@ export const mapBasket = async(basketName, adminId, customerId, broker, quantity
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({
+            body: encrypt(JSON.stringify({
                 "basketName": basketName,
                 "adminId": adminId,
                 "customerId": customerId,
                 "brokerName": broker,
                 "basketUnits": quantity
-            })
+            }))
         }
         const response = await fetch(`http://${DOMAIN}:${PORT}/customer/map`, requestOptions);
-        if(response.status === 200) {
-            const data = await response.text();
-            return data;
-        } else {
-            const data = await response.text();
-            return data;
-        }
+        const status = response.status;
+
+        const jsonData = await response.json();
+        const data = decrypt(jsonData.payload);
+        return { status, data };
     }
     catch(error){
-        console.log(error);
-        return false;
+        errorLogger(error);
     }
 }
 
@@ -40,26 +41,23 @@ export const unMapBasket = async(basketName, adminId, customerId, broker, quanti
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({
+            body: encrypt(JSON.stringify({
                 "basketName": basketName,
                 "adminId": adminId,
                 "customerId": customerId,
                 "brokerName": broker,
                 "basketUnits": quantity
-            })
+            }))
         }
         const response = await fetch(`http://${DOMAIN}:${PORT}/customer/single/unmap`, requestOptions);
-        if(response.status === 200) {
-            const data = await response.text();
-            return data;
-        } else {
-            const data = await response.text();
-            return data;
-        }
+        const status = response.status;
+
+        const jsonData = await response.json();
+        const data = decrypt(jsonData.payload);
+        return { status, data };
     }
     catch(error){
-        console.log(error);
-        return false;
+        errorLogger(error);
     }
 }
 
@@ -71,27 +69,23 @@ export const sendWeblink = async(basketName, adminId, customerId, brokerName, ba
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({
+            body: encrypt(JSON.stringify({
                 "basketName": basketName,
                 "adminId": adminId,
                 "customerId": customerId,
                 "brokerName": brokerName,
                 "basketUnits": basketUnits,
-            })
+            }))
         }
         const response = await fetch(`http://${DOMAIN}:${PORT}/send/weblink`, requestOptions);
+        const status = response.status;
 
-        if(response.status === 200){
-            const responseText = await response.text();
-            return responseText;
-        }
-        else{
-            const data = await response.text();
-            return data;
-        }
+        const jsonData = await response.json();
+        const data = decrypt(jsonData.payload);
+        return { status, data };
     }
     catch(error){
-        return false;
+        errorLogger(error);
     }
 }
 
@@ -103,22 +97,19 @@ export const getCustomerStatus = async (basketName) => {
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({
+            body: encrypt(JSON.stringify({
                 "basketName": basketName,
-            })
+            }))
         }
         const response = await fetch(`http://${DOMAIN}:${PORT}/mappedstatus`, requestOptions);
+        const status = response.status;
 
-        if(response.status === 200){
-            const responseText = await response.json();
-            return responseText;
-        }
-        else{
-            return [];
-        }
+        const jsonData = await response.json();
+        const data = decrypt(jsonData.payload);
+        return { status, data };
     }
     catch(error){
-        return [];
+        errorLogger(error);
     }
 }
 
